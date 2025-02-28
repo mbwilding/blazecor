c30754a526808983ef62a1a319a9489d04689399
Matthew Wilding <mbwilding@gmail.com>
2025-03-01 04:00:11 +0800

    2025-03-01-04-00-11

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
+import { KeymapDEFY } from "./components/dygma/layouts/defy";
 
 // TODO: Move
 document.documentElement.classList.add("dark");
@@ -18,38 +19,39 @@ function App() {
     }
 
     return (
-        <main className="container">
-            <h1>Welcome to Tauri + React</h1>
-
-            <div className="row">
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-                </a>
-                <a href="https://tauri.app" target="_blank">
-                    <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-            <form
-                className="row"
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    greet();
-                }}
-            >
-                <Input
-                    id="greet-input"
-                    onChange={(e) => setName(e.currentTarget.value)}
-                    placeholder="Enter a name..."
-                />
-                <Button type="submit">Greet</Button>
-            </form>
-            <p>{greetMsg}</p>
-        </main>
+        <KeymapDEFY index={-1} darkMode={true} showUnderglow={true} isStandardView={false} onKeySelect={(e) => console.log(e)} />
+        // <main className="container">
+        //     <h1>Welcome to Tauri + React</h1>
+        //
+        //     <div className="row">
+        //         <a href="https://vitejs.dev" target="_blank">
+        //             <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        //         </a>
+        //         <a href="https://tauri.app" target="_blank">
+        //             <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        //         </a>
+        //         <a href="https://reactjs.org" target="_blank">
+        //             <img src={reactLogo} className="logo react" alt="React logo" />
+        //         </a>
+        //     </div>
+        //     <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+        //
+        //     <form
+        //         className="row"
+        //         onSubmit={(e) => {
+        //             e.preventDefault();
+        //             greet();
+        //         }}
+        //     >
+        //         <Input
+        //             id="greet-input"
+        //             onChange={(e) => setName(e.currentTarget.value)}
+        //             placeholder="Enter a name..."
+        //         />
+        //         <Button type="submit">Greet</Button>
+        //     </form>
+        //     <p>{greetMsg}</p>
+        // </main>
     );
 }
 
