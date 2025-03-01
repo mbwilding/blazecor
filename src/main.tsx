0cc2a34aac336204736d6b76328ca40ee2df0286
Matthew Wilding <mbwilding@gmail.com>
2025-03-01 13:46:42 +0800

    2025-03-01-13-46-42

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { warn, debug, trace, info, error } from '@tauri-apps/plugin-log';
+
+function forwardConsole(
+    fnName: 'log' | 'debug' | 'info' | 'warn' | 'error',
+    logger: (message: string) => Promise<void>
+) {
+    const original = console[fnName];
+    console[fnName] = (message) => {
+        original(message);
+        logger(message);
+    };
+}
+
+forwardConsole('log', trace);
+forwardConsole('debug', debug);
+forwardConsole('info', info);
+forwardConsole('warn', warn);
+forwardConsole('error', error);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
