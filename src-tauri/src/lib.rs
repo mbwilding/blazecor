1e8252cf3fb87e9215c914d89a444819f89b834d
Matthew Wilding <mbwilding@gmail.com>
2025-03-01 15:38:30 +0800

    2025-03-01-15-38-30

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
-use tauri::Result;
 use dygma_focus::prelude::*;
+use tauri::Result;
 
 // #[tauri::command]
 // fn greet(name: &str) -> String {
@@ -21,22 +21,19 @@ async fn version() -> Result<String> {
     Ok(version)
 }
 
-// #[tauri::command]
-// async fn settings_get() -> Result<String> {
-//     let mut focus = Focus::new_first_available()?;
-//     let settings = focus.settings_get().await?;
-//     Ok(serde_json::to_string(&settings)?)
-// }
+#[tauri::command]
+async fn settings_get() -> Result<String> {
+    let mut focus = Focus::new_first_available()?;
+    let settings = focus.settings_get().await?;
+    Ok(serde_json::to_string(&settings)?)
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
-        // .invoke_handler(tauri::generate_handler![greet])
-        .invoke_handler(tauri::generate_handler![devices])
-        .invoke_handler(tauri::generate_handler![version])
-        // .invoke_handler(tauri::generate_handler![settings_get])
+        .invoke_handler(tauri::generate_handler![devices, version, settings_get])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
