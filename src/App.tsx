b2210bb7880cc8fd2155270f345b3b338b0ae6f0
Matthew Wilding <mbwilding@gmail.com>
2025-03-02 15:38:37 +0800

    2025-03-02-15-38-37

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import PageColors from "./pages/pageColors";
 import { Device } from "./types/ffi/hardware"; // Assuming Device is properly defined in this path
 import { Button } from "@/components/ui/button";
 import { useConnect, useDevices, useSettings, useVersion } from "./Api";
+import {
+    Dialog,
+    DialogTrigger,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog";
 
+// TODO: Move
 document.documentElement.classList.add("dark");
 
 function useDeviceConnection(quickConnect: boolean) {
@@ -41,29 +49,47 @@ interface DeviceConnectionProps {
     fetchDevices: () => void;
 }
 
-function DeviceConnection({ devices, handleDeviceSelection, fetchDevices }: DeviceConnectionProps) {
+function DeviceConnection({
+    devices,
+    handleDeviceSelection,
+    fetchDevices,
+}: DeviceConnectionProps) {
+    const [open, setOpen] = useState(false);
+
+    const handleOpenChange = (isOpen: boolean) => {
+        setOpen(isOpen);
+        if (isOpen) {
+            fetchDevices();
+        }
+    };
+
     return (
-        <div className="container flex flex-col justify-center items-center">
-            <form
-                className="row"
-                onSubmit={e => {
-                    e.preventDefault();
-                    fetchDevices();
-                }}
-            >
-                <Button type="submit">Devices</Button>
-            </form>
-            {devices?.map((device, index) => (
-                <Button key={index} type="button" onClick={() => handleDeviceSelection(device)}>
-                    {device.hardware.info.displayName}
-                </Button>
-            ))}
-        </div>
+        <Dialog open={open} onOpenChange={handleOpenChange}>
+            <DialogTrigger asChild>
+                <Button>Devices</Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Select a Device</DialogTitle>
+                </DialogHeader>
+                <div className="flex flex-col space-y-2">
+                    {devices?.map((device, index) => (
+                        <Button
+                            key={index}
+                            variant="ghost"
+                            onClick={() => handleDeviceSelection(device)}
+                        >
+                            {device.hardware.info.displayName}
+                        </Button>
+                    ))}
+                </div>
+            </DialogContent>
+        </Dialog>
     );
 }
 
 function App() {
-    const { device, version, settings, devices, handleDeviceSelection, fetchDevices } = useDeviceConnection(true);
+    const { device, version, settings, devices, handleDeviceSelection, fetchDevices } = useDeviceConnection(false);
 
     return (
         <main className="container">
