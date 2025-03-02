91c6ef87685542e29b179de2160d35b999805df7
Matthew Wilding <mbwilding@gmail.com>
2025-03-02 22:21:48 +0800

    2025-03-02-22-21-48

--- src/pages/pageColors.tsx
+++ src/pages/pageColors.tsx
@@ -2,12 +2,15 @@ import { LayoutDefy } from "@/components/dygma/layouts/defy";
 import { ColorPalette } from "@/components/custom/color-palette";
 import { Color, Settings } from "@/types/ffi/settings";
 import { LayerSelector } from "@/components/custom/layer-selector";
+import { useState } from "react";
 
 export interface PageColorsProps {
     settings: Settings;
 }
 
 export default function PageColors({ settings }: PageColorsProps) {
+    const [selectedLayer, setSelectedLayer] = useState(settings.settingsDefaultlayer);
+
     let palette = settings.paletteRgb || settings.paletteRgbw;
     let colorMap = settings.colorMap;
 
@@ -17,12 +20,16 @@ export default function PageColors({ settings }: PageColorsProps) {
         }
     };
 
+    const handleSelectedLayerChange = (index: number) => {
+        setSelectedLayer(index);
+    };
+
     return (
         <div className="flex flex-col justify-center items-center">
             <ColorPalette colors={palette || []} onChange={handleSelectedColorChange} />
-            <LayerSelector defaultLayer={settings.settingsDefaultlayer} layers={10} />
+            <LayerSelector defaultLayer={settings.settingsDefaultlayer} layers={10} onChange={handleSelectedLayerChange} />
             <LayoutDefy
-                layer={0}
+                layer={selectedLayer}
                 darkMode={true}
                 showUnderglow={true}
                 isStandardView={false}
