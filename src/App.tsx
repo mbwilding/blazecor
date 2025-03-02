63f23551a47caf7ed8e73b1dfc7189f9eefdfbb9
Matthew Wilding <mbwilding@gmail.com>
2025-03-02 14:37:47 +0800

    2025-03-02-14-37-47

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import "./App.css";
 import { Button } from "./components/ui/button";
 import { Device } from "./types/ffi/hardware";
 import { useConnect, useDevices, useSettings, useVersion } from "./Api";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PageColors from "./components/pages/pageColors";
 
-// TODO: Move
+// TODO: Move these
 document.documentElement.classList.add("dark");
+const quickConnect = true;
 
 function App() {
     const [device, setDevice] = useState<Device>();
@@ -17,28 +18,42 @@ function App() {
     const version = useVersion(device);
     const settings = useSettings(device);
 
+    useEffect(() => {
+        if (quickConnect && !devices) {
+            fetchDevices();
+        }
+    }, [quickConnect, devices, fetchDevices]);
+
+    useEffect(() => {
+        if (quickConnect && devices && devices.length > 0 && !device) {
+            setDevice(devices[0]);
+        }
+    }, [quickConnect, devices, device]);
+
     return (
         <main className="container">
             {device && settings ? (
                 <PageColors settings={settings} />
             ) : (
-                <>
-                    <form
-                        className="row"
-                        onSubmit={e => {
-                            e.preventDefault();
-                            fetchDevices();
-                        }}
-                    >
-                        <Button type="submit">Devices</Button>
-                    </form>
+                !quickConnect && (
+                    <>
+                        <form
+                            className="row"
+                            onSubmit={e => {
+                                e.preventDefault();
+                                fetchDevices();
+                            }}
+                        >
+                            <Button type="submit">Devices</Button>
+                        </form>
 
-                    {devices?.map((device, index) => (
-                        <Button key={index} type="button" onClick={() => setDevice(device)}>
-                            {device.hardware.info.displayName}
-                        </Button>
-                    ))}
-                </>
+                        {devices?.map((device, index) => (
+                            <Button key={index} type="button" onClick={() => setDevice(device)}>
+                                {device.hardware.info.displayName}
+                            </Button>
+                        ))}
+                    </>
+                )
             )}
         </main>
     );
