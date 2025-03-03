6a440dd921ed0e7255909207ffc08b22959c2a34
Matthew Wilding <mbwilding@gmail.com>
2025-03-03 21:27:13 +0800

    2025-03-03-21-27-13

--- src/utils/rgbwToRgb.ts
+++ src/utils/rgbwToRgb.ts
@@ -1,9 +1,18 @@
 import { RGB, RGBW } from "@/types/ffi/settings";
 
 export function rgbwToRgb(rgbw: RGBW): RGB {
+    const r = rgbw.r / 255;
+    const g = rgbw.g / 255;
+    const b = rgbw.b / 255;
+    const w = rgbw.w / 255 * 0.75; // The white LED is about 75% as bright
+
+    const rNew = r * (1 - w) + w;
+    const gNew = g * (1 - w) + w;
+    const bNew = b * (1 - w) + w;
+
     return {
-        r: Math.min(255, rgbw.r + rgbw.w),
-        g: Math.min(255, rgbw.g + rgbw.w),
-        b: Math.min(255, rgbw.b + rgbw.w),
+        r: Math.round(rNew * 255),
+        g: Math.round(gNew * 255),
+        b: Math.round(bNew * 255),
     };
 }
