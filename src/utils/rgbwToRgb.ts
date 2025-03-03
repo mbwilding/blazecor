2117557f68b1e402310c5d9f51a21904bfa80de9
Matthew Wilding <mbwilding@gmail.com>
2025-03-03 21:28:19 +0800

    2025-03-03-21-28-18

--- src/utils/rgbwToRgb.ts
+++ src/utils/rgbwToRgb.ts
@@ -1,10 +1,12 @@
 import { RGB, RGBW } from "@/types/ffi/settings";
 
+const whiteScalar = 0.75; // The white LED is about 75% as bright
+
 export function rgbwToRgb(rgbw: RGBW): RGB {
     const r = rgbw.r / 255;
     const g = rgbw.g / 255;
     const b = rgbw.b / 255;
-    const w = rgbw.w / 255 * 0.75; // The white LED is about 75% as bright
+    const w = rgbw.w / 255 * whiteScalar;
 
     const rNew = r * (1 - w) + w;
     const gNew = g * (1 - w) + w;
