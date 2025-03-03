1ede64452c4ee1149066e8c5d0fe00b379009d96
Matthew Wilding <mbwilding@gmail.com>
2025-03-03 21:36:22 +0800

    2025-03-03-21-36-22

--- src/utils/colorConverters.ts
+++ src/utils/colorConverters.ts
@@ -1,12 +1,12 @@
 import { RGB, RGBW } from "@/types/ffi/settings";
 
-const whiteScalar = 0.75; // The white LED is about 75% as bright
+export const WHITE_SCALAR = 0.75; // The white LED is about 75% as bright
 
 export function rgbwToRgb(rgbw: RGBW): RGB {
     const r = rgbw.r / 255;
     const g = rgbw.g / 255;
     const b = rgbw.b / 255;
-    const w = (rgbw.w / 255) * whiteScalar;
+    const w = (rgbw.w / 255) * WHITE_SCALAR;
 
     const rNew = r * (1 - w) + w;
     const gNew = g * (1 - w) + w;
