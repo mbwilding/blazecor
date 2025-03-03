8777585c2e127269e283a81a1a55193b81ec1cae
Matthew Wilding <mbwilding@gmail.com>
2025-03-03 20:33:59 +0800

    2025-03-03-20-33-59

--- src/components/custom/color-palette.tsx
+++ src/components/custom/color-palette.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { RGB, RGBW } from "@/types/ffi/settings";
+import { RGBW } from "@/types/ffi/settings";
 import ColorPicker from "./color-picker";
 
 interface ColorPaletteProps {
